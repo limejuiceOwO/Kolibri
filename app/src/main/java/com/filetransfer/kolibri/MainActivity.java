990e47783bc79fd1a652bb11a13f8f4144635e38
limejuiceOwO <limejuice1536@hotmail.com>
2023-04-30 13:43:50 +0800

    add permission handler

--- app/src/main/java/com/filetransfer/kolibri/MainActivity.java
+++ app/src/main/java/com/filetransfer/kolibri/MainActivity.java
@@ -1,20 +1,35 @@
 package com.filetransfer.kolibri;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.net.wifi.WifiManager;
-import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.view.View;
+import android.Manifest;
+import android.util.Log;
 
 import com.filetransfer.kolibri.ui.main.MainFragment;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERM_CODE = 1;
+    private static final ArrayList<String> requiredPermissions = new ArrayList<>();
+
+    static {
+        requiredPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        requiredPermissions.add(Manifest.permission.READ_PHONE_STATE);
+//        requiredPermissions.add(Manifest.permission.CHANGE_WIFI_STATE);
+//        requiredPermissions.add(Manifest.permission.CHANGE_NETWORK_STATE);
+//        requiredPermissions.add(Manifest.permission.REQUEST_INSTALL_PACKAGES);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requiredPermissions.add(Manifest.permission.QUERY_ALL_PACKAGES);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +39,59 @@ public class MainActivity extends AppCompatActivity {
 //        StrictMode.setThreadPolicy(policy);
 
         setContentView(R.layout.activity_main);
+
+        if (!checkAndRequirePermissions()) {
+            return;
+        }
+
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new MainFragment()).commit();
+            launchMainFragment();
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode != PERM_CODE) {
+            return;
+        }
+
+        for (int res : grantResults) {
+            if (res != PackageManager.PERMISSION_GRANTED) {
+                AlertDialog dlg = new AlertDialog.Builder(this)
+                        .setTitle("Permission Denied")
+                        .setMessage("Please grant all permissions to use this app")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", (d, which) -> checkAndRequirePermissions())
+                        .setNegativeButton("Exit", (d, which) -> finish())
+                        .create();
+                dlg.show();
+                return;
+            }
+        }
+
+        // all permissions are granted
+        launchMainFragment();
+    }
+
+    private void launchMainFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new MainFragment()).commit();
+    }
+
+    private boolean checkAndRequirePermissions() {
+        ArrayList<String> permissionNeeded = new ArrayList<>();
+        for (String perm : requiredPermissions) {
+            if (checkSelfPermission(perm) != PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "permission needed: " + perm);
+                permissionNeeded.add(perm);
+            }
+        }
+
+        if (permissionNeeded.size() == 0) {
+            return true;
+        }
+
+        requestPermissions(requiredPermissions.toArray(new String[0]), PERM_CODE);
+        return false;
+    }
 }
